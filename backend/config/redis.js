f919cbbbb6df5db663dff9c5199eecdbe995c599
Arukala Raju <arukalaraju13@gmail.com>
2026-07-14 01:48:07 +0530

    Fix Upstash TLS Redis connection

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -2,9 +2,7 @@
 
 // import Redis from "ioredis";
 
-// export const connection = new Redis({
-//   host: "localhost",
-//   port: 6379,
+// export const connection = new Redis(process.env.REDIS_URL, {
 //   maxRetriesPerRequest: null,
 // });
 
@@ -12,9 +10,18 @@
 //   console.log("✅ Redis Connected");
 // });
 
+// connection.on("ready", () => {
+//   console.log("🚀 Redis Ready");
+// });
+
 // connection.on("error", (err) => {
 //   console.log("❌ Redis Error:", err.message);
 // });
+
+// connection.on("close", () => {
+//   console.log("⚠️ Redis Connection Closed");  
+// });     
+
 
 
 
@@ -22,6 +29,7 @@ import Redis from "ioredis";
 
 export const connection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
+  tls: {},
 });
 
 connection.on("connect", () => {
