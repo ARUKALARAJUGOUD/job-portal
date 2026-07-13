e962d5d8ef0da7e8496fa607c648419460339780
Arukala Raju <arukalaraju13@gmail.com>
2026-07-14 01:21:39 +0530

    Connect Redis using Upstash in production

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,10 +1,26 @@
 
 
+// import Redis from "ioredis";
+
+// export const connection = new Redis({
+//   host: "localhost",
+//   port: 6379,
+//   maxRetriesPerRequest: null,
+// });
+
+// connection.on("connect", () => {
+//   console.log("✅ Redis Connected");
+// });
+
+// connection.on("error", (err) => {
+//   console.log("❌ Redis Error:", err.message);
+// });
+
+
+
 import Redis from "ioredis";
 
-export const connection = new Redis({
-  host: "localhost",
-  port: 6379,
+export const connection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null,
 });
 
@@ -12,6 +28,14 @@ connection.on("connect", () => {
   console.log("✅ Redis Connected");
 });
 
+connection.on("ready", () => {
+  console.log("🚀 Redis Ready");
+});
+
 connection.on("error", (err) => {
   console.log("❌ Redis Error:", err.message);
+});
+
+connection.on("close", () => {
+  console.log("⚠️ Redis Connection Closed");
 });
